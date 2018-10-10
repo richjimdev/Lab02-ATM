5362afd95e708616d17e8762ed13a2069a9f8677
Richard J <richjim89@gmail.com>
2018-10-10 13:41:36 -0700

    withdraw method

--- bankATM/bankATM/Program.cs
+++ bankATM/bankATM/Program.cs
@@ -15,28 +15,32 @@ namespace bankATM
                 Console.WriteLine("2. Withdraw");
                 Console.WriteLine("3. Deposit");
                 Console.WriteLine("4. Exit");
-
-                bool skipFinally = false;
-
-
+                
                 try
                 {
                     int selection = int.Parse(Console.ReadLine());
-                    if (selection == 1) Console.WriteLine($"Your Balance is {GetBalance()}");
+                    if (selection == 1) Console.WriteLine($"Your Balance is ${GetBalance()}");
                     else if (selection == 2)
                     {
-                        Console.WriteLine($"How much would you like to withdrawl? Current balance is {GetBalance()}");
+                        Console.WriteLine($"How much would you like to withdrawl? Current balance is ${GetBalance()}");
                         decimal ammount = decimal.Parse(Console.ReadLine());
                         if (ammount <= 0) throw new Exception("NegativeNumberInput");
+                        Withdraw(ammount);
+                        Console.WriteLine($"Your new balance is now ${ GetBalance() }");
+
                     }
                     else if (selection == 4) Environment.Exit(0);
                     else throw new ArgumentException("InvalidATMSelection");
                 }
-                catch (ArgumentException e)
+                catch (ArgumentException)
                 {
                     Console.WriteLine("Please select options 1 - 4 only.");
                 }
-                catch (Exception e)
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Not enough money available in your account");
+                }
+                catch (Exception)
                 {
                     Console.WriteLine("Please only input a positive number");
                 }
@@ -61,6 +65,12 @@ namespace bankATM
         public static decimal GetBalance()
         {
             return balance;
+        }
+
+        public static decimal Withdraw(decimal ammount)
+        {
+            if ((balance - ammount) < 0) throw new IndexOutOfRangeException("NotEnoughMoney");
+            return balance = balance - ammount;
         }
     }
 }
