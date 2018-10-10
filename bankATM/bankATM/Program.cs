c2c7d93efb176278e5eb94bcecb606eaab146589
Richard J <richjim89@gmail.com>
2018-10-10 11:41:44 -0700

    added finally block

--- bankATM/bankATM/Program.cs
+++ bankATM/bankATM/Program.cs
@@ -16,12 +16,20 @@ namespace bankATM
                 Console.WriteLine("3. Deposit");
                 Console.WriteLine("4. Exit");
 
+                bool skipFinally = false;
+
 
                 try
                 {
                     int selection = int.Parse(Console.ReadLine());
                     if (selection == 1) Console.WriteLine($"Your Balance is {GetBalance()}");
-                    else if (selection == 4) break;
+                    else if (selection == 2)
+                    {
+                        Console.WriteLine($"How much would you like to withdrawl? Current balance is {GetBalance()}");
+                        decimal ammount = decimal.Parse(Console.ReadLine());
+                        if (ammount <= 0) throw new Exception("NegativeNumberInput");
+                    }
+                    else if (selection == 4) Environment.Exit(0);
                     else throw new ArgumentException("InvalidATMSelection");
                 }
                 catch (ArgumentException e)
@@ -31,6 +39,19 @@ namespace bankATM
                 catch (Exception e)
                 {
                     Console.WriteLine("Please only input a positive number");
+                }
+                finally
+                {
+                    Console.WriteLine("Would you like to perform another transaction? y/n:");
+                    string userAnswer = Console.ReadLine().ToLower();
+                    while (userAnswer != "y")
+                    {
+                        if (userAnswer == "n")
+                            Environment.Exit(0);
+                        else Console.WriteLine("Please only enter y or n:");
+                        userAnswer = Console.ReadLine().ToLower();
+                    }
+
                 }
 
             }
