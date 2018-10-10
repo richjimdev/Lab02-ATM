51f29d36edc4eb744127316937032ffce8c0f79f
Richard J <richjim89@gmail.com>
2018-10-10 10:41:06 -0700

    setup inital interaction

---
+++ bankATM/bankATM/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace bankATM
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            decimal balance = 500.00m;
+            while(true)
+            {
+                Console.WriteLine("Welcome to 401 bank! Select an option.");
+                Console.WriteLine("1. View Balance");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Deposit");
+                Console.WriteLine("4. Exit");
+
+
+                try
+                {
+                    decimal selection = decimal.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Please input only positive numbers");
+                }
+
+            }
+        }
+
+    }
+}
+
---
+++ bankATM/lab02-ATM-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static bankATM.Program;
+
+namespace lab02_ATM_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
