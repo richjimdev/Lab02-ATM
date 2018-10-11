806d70f5c35b837333c9c71b32efb15653b154e3
Richard J <richjim89@gmail.com>
2018-10-10 18:01:46 -0700

    added comments

--- bankATM/bankATM/Program.cs
+++ bankATM/bankATM/Program.cs
@@ -4,10 +4,12 @@ namespace bankATM
 {
     public class Program
     {
+        // Storing balance to be accessed by all methods
         public static decimal balance = 500.00m;
 
         public static void Main(string[] args)
         {
+            //Loop that will run until user decides to exit
             while(true)
             {
                 Console.WriteLine("Welcome to 401 bank! Select an option.");
@@ -16,6 +18,7 @@ namespace bankATM
                 Console.WriteLine("3. Deposit");
                 Console.WriteLine("4. Exit");
                 
+                // Multiple blocks of code can cause exceptions therefore I'm adding my code here
                 try
                 {
                     int selection = int.Parse(Console.ReadLine());
@@ -24,6 +27,7 @@ namespace bankATM
                     {
                         Console.WriteLine($"How much would you like to withdrawl? Current balance is ${GetBalance()}");
                         decimal ammount = decimal.Parse(Console.ReadLine());
+                        // This prevents user from withdrawing more than what is available
                         if (ammount <= 0) throw new Exception("NegativeNumberInput");
                         Withdraw(ammount);
                         Console.WriteLine($"Your new balance is now ${ GetBalance() }");
@@ -33,13 +37,16 @@ namespace bankATM
                     {
                         Console.WriteLine($"How much would you like to deposit? Current balance is ${GetBalance()}");
                         decimal ammount = decimal.Parse(Console.ReadLine());
+                        // This prevents user from entering negative numbers for a deposit
                         if (ammount <= 0) throw new Exception("NegativeNumberInput");
                         Deposit(ammount);
                         Console.WriteLine($"Your new balance is now ${ GetBalance() }");
                     }
                     else if (selection == 4) Environment.Exit(0);
+                    // If an ivalid choice, will throw an exception
                     else throw new ArgumentException("InvalidATMSelection");
                 }
+                // Set up my throw and catch statements to do specific tasks depending on the type of exception thrown
                 catch (ArgumentException)
                 {
                     Console.WriteLine("Please select options 1 - 4 only.");
@@ -54,6 +61,7 @@ namespace bankATM
                 }
                 finally
                 {
+                    // Prompts user whether there were errors or not
                     Console.WriteLine("Would you like to perform another transaction? y/n:");
                     string userAnswer = Console.ReadLine().ToLower();
                     while (userAnswer != "y")
